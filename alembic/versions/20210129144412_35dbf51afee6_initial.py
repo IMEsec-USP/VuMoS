f45342bf6b9758ed7e8d57f7495dac0c6fa18d1b
Cainotis <cainotis@gmail.com>
2021-02-01 15:13:51 -0300

    Added initial migration to Data Base with alembic
    - updated requirements.txt
    - added configuration to alembic and the first revision

---
+++ alembic/versions/20210129144412_35dbf51afee6_initial.py
@@ -0,0 +1,90 @@
+"""'initial'
+
+Revision ID: 35dbf51afee6
+Revises: 
+Create Date: 2021-01-29 14:44:12.594927
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '35dbf51afee6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+	op.create_table('host',
+		sa.Column('host_id', sa.Integer(), autoincrement=True),
+		sa.Column('domain', sa.String(length=128), nullable=False),
+		sa.Column('added_dttm', postgresql.TIMESTAMP(), nullable=False, server_defalt='now()'),
+		sa.Column('access_dttm', postgresql.TIMESTAMP(), nullable=False, server_defalt='now()'),
+		sa.Column('times_offline', sa.SMALLINT(), nullable=False, server_defalt='0'),
+		sa.Column('updated_dttm', postgresql.TIMESTAMP(), nullable=False, server_defalt='now()'),
+		sa.PrimaryKeyConstraint('host_id')
+	)
+	op.create_index(op.f('ix_host_domain'), 'host', ['domain'], unique=True)
+	op.execute("""
+		ALTER TABLE ONLY host ALTER column added_dttm SET default now();
+		ALTER TABLE ONLY host ALTER column access_dttm SET default now();
+		ALTER TABLE ONLY host ALTER column updated_dttm SET default now();
+		ALTER TABLE ONLY host ALTER column times_offline SET default 0;
+	""")
+
+	op.create_table('machine',
+		sa.Column('machine_id', sa.Integer(), nullable=False),
+		sa.Column('ip', postgresql.INET(), nullable=False),
+		sa.Column('institute', sa.TEXT(), nullable=True),
+		sa.Column('external', sa.Boolean(), nullable=False, server_defalt='false'),
+		sa.Column('updated_dttm', postgresql.TIMESTAMP(), nullable=False, server_defalt='now()'),
+		sa.PrimaryKeyConstraint('machine_id')
+	)
+	op.create_index(op.f('ix_machine_ip'), 'machine', ['ip'], unique=True)
+	op.execute("""
+		ALTER TABLE ONLY machine ALTER column "external" SET DEFAULT false;
+		ALTER TABLE ONLY machine ALTER column updated_dttm SET DEFAULT now();
+	""")
+	
+	op.create_table('link',
+		sa.Column('link_id', sa.Integer(), nullable=False),
+		sa.Column('host_id', sa.Integer(), nullable=False),
+		sa.Column('url', sa.String(length=128), nullable=False),
+		sa.Column('added_dttm', postgresql.TIMESTAMP(), nullable=False, server_defalt='now()'),
+		sa.Column('access_dttm', postgresql.TIMESTAMP(), nullable=False, server_defalt='now()'),
+		sa.Column('times_offline', sa.SMALLINT(), nullable=False, server_defalt='0'),
+		sa.Column('updated_dttm', postgresql.TIMESTAMP(), nullable=False, server_defalt='now()'),
+		sa.ForeignKeyConstraint(['host_id'], ['host.host_id'], ),
+		sa.PrimaryKeyConstraint('link_id')
+	)
+	op.create_index(op.f('ix_link_url'), 'link', ['url'], unique=True)
+	op.execute("""
+		ALTER TABLE ONLY link ALTER column added_dttm SET default now();
+		ALTER TABLE ONLY link ALTER column access_dttm SET default now();
+		ALTER TABLE ONLY link ALTER column updated_dttm SET default now();
+		ALTER TABLE ONLY link ALTER column times_offline SET default 0;
+	""")
+	
+	op.create_table('machine_host',
+		sa.Column('host_id', sa.Integer(), nullable=False),
+		sa.Column('machine_id', sa.Integer(), nullable=False),
+		sa.Column('updated_dttm', postgresql.TIMESTAMP(), nullable=False, server_defalt='now()'),
+		sa.ForeignKeyConstraint(['host_id'], ['host.host_id'], ),
+		sa.ForeignKeyConstraint(['machine_id'], ['machine.machine_id'], ),
+		sa.PrimaryKeyConstraint('host_id', 'machine_id')
+	)
+	op.execute("""
+		ALTER TABLE ONLY machine_host ALTER column updated_dttm SET default now();
+	""")
+
+
+def downgrade():
+	op.drop_table('machine_host')
+	op.drop_index(op.f('ix_link_url'), table_name='link')
+	op.drop_table('link')
+	op.drop_index(op.f('ix_machine_ip'), table_name='machine')
+	op.drop_table('machine')
+	op.drop_index(op.f('ix_host_domain'), table_name='host')
+	op.drop_table('host')
