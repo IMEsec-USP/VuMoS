89b84ed8fe8ccfaf54bf946bd8e3bd8be54a2234
Cainotis <cainotis@gmail.com>
2021-02-12 14:48:11 -0300

    Added new tables
    - added tables vulnerability and vulnerability_type
    - added alembic revision
    - added their definitions to mapper

--- alchemyrepository/_mapper.py
+++ alchemyrepository/_mapper.py
@@ -1,6 +1,7 @@
 from sqlalchemy import \
 	Boolean, \
 	Column, \
+	Enum, \
 	ForeignKey, \
 	Integer, \
 	MetaData, \
@@ -28,10 +29,12 @@ Base = MetaData()
 
 class Mapper(object):
 	def __init__(self):
-		self.machine = None
 		self.host = None
+		self.machine = None
 		self.machine_host = None
 		self.path = None
+		self.vulnerability = None
+		self.vulnerability_type = None
 		self.map()
 
 	def map(self):
@@ -41,27 +44,7 @@ class Mapper(object):
 			Column("host_id", Integer, ForeignKey("host.host_id"), nullable=False, primary_key=True),
 			Column("machine_id", Integer, ForeignKey("machine.machine_id"), nullable=False, primary_key=True),
 			Column("updated_dttm", TIMESTAMP, server_default='now()', nullable=False),
-			# schema=""
 		)
-
-		self.machine = Table(
-			"machine",
-			Base,
-			Column("machine_id", Integer, primary_key=True),
-			Column("ip", INET, unique=True, nullable=False, index=True),
-			Column("institute", TEXT),
-			Column("external", Boolean, nullable=False, server_default='false'),
-			Column("updated_dttm", TIMESTAMP, server_default='now()', nullable=False),
-			# schema=""
-		)
-		mapper(Machine, self.machine, properties={
-			"id": self.machine.c.machine_id,
-			"ip": self.machine.c.ip,
-			"hosts": relationship(Host, secondary=self.machine_host, back_populates="machines"),
-			"external": self.machine.c.external,
-			"institute": self.machine.c.institute,
-			"updated_dttm": self.machine.c.updated_dttm
-		})
 
 		self.host = Table(
 			"host",
@@ -72,7 +55,6 @@ class Mapper(object):
 			Column("access_dttm", TIMESTAMP, server_default='now()', nullable=False),
 			Column("times_offline", SMALLINT, server_default='0', nullable=False),
 			Column("updated_dttm", TIMESTAMP, server_default='now()', nullable=False),
-			# schema=""
 		)
 		mapper(Host, self.host, properties={
 			"id": self.host.c.host_id,
@@ -84,7 +66,24 @@ class Mapper(object):
 			"updated_dttm": self.host.c.updated_dttm
 		})
 
-		
+		self.machine = Table(
+			"machine",
+			Base,
+			Column("machine_id", Integer, primary_key=True),
+			Column("ip", INET, unique=True, nullable=False, index=True),
+			Column("institute", TEXT),
+			Column("external", Boolean, nullable=False, server_default='false'),
+			Column("updated_dttm", TIMESTAMP, server_default='now()', nullable=False),
+		)
+		mapper(Machine, self.machine, properties={
+			"id": self.machine.c.machine_id,
+			"ip": self.machine.c.ip,
+			"hosts": relationship(Host, secondary=self.machine_host, back_populates="machines"),
+			"external": self.machine.c.external,
+			"institute": self.machine.c.institute,
+			"updated_dttm": self.machine.c.updated_dttm
+		})
+
 		self.path = Table(
 			"path",
 			Base,
@@ -97,7 +96,6 @@ class Mapper(object):
 			Column("access_dttm", TIMESTAMP, server_default='now()', nullable=False),
 			Column("times_offline", SMALLINT, server_default='0', nullable=False),
 			Column("updated_dttm", TIMESTAMP, server_default='now()', nullable=False),
-			# schema=""
 		)
 		mapper(Path, self.path, properties={
 			"id": self.path.c.path_id,
@@ -109,5 +107,32 @@ class Mapper(object):
 			"times_offline": self.path.c.times_offline,
 			"updated_dttm": self.path.c.updated_dttm
 		})
+
+		self.vulnerability = Table(
+			"vulnerability",
+			Base,
+			Column("vulnerability_id", Integer, primary_key=True),
+			Column("vulnerability_type_id", ForeignKey("vulnerability_type.vulnerability_type_id"), nullable=False),
+			Column("status", Enum("found", "confirmed", "solved", "false positive", name="vulnerability_status_enum"), nullable=False),
+			Column("found_by", TEXT, nullable=False),
+			Column("found_dttm", TIMESTAMP, server_default='now()', nullable=False),
+			Column("confirmed_by", TEXT),
+			Column("confirmed_dttm", TIMESTAMP),
+			Column("solved_dttm", TIMESTAMP),
+			Column("host_id", Integer, ForeignKey(self.host.c.host_id), nullable=False),
+			Column("path_id", Integer, ForeignKey(self.path.c.path_id), nullable=False),
+			Column("machine_id", Integer, ForeignKey(self.machine.c.machine_id), nullable=False),
+			Column("updated_dttm", TIMESTAMP, server_default='now()', nullable=False),
+		)
+
+		self.vulnerability_type = Table(
+			"vulnerability_type",
+			Base,
+			Column("vulnerability_type_id", Integer, primary_key=True),
+			Column("name", TEXT, unique=True, nullable=False, index=True),
+			Column("description", TEXT),
+			Column("severity", SMALLINT, nullable=False, index=True),
+			Column("updated_dttm", TIMESTAMP, server_default='now()', nullable=False),
+		)
 
 Mapper()
---
+++ alembic/versions/20210212143516_d13f5a4d0eca_adding_vunerability.py
@@ -0,0 +1,69 @@
+"""adding vunerability
+
+Revision ID: d13f5a4d0eca
+Revises: 35dbf51afee6
+Create Date: 2021-02-12 14:35:16.996057
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = 'd13f5a4d0eca'
+down_revision = '35dbf51afee6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('vulnerability_type',
+        sa.Column('vulnerability_type_id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.TEXT(), nullable=False),
+        sa.Column('description', sa.TEXT(), nullable=True),
+        sa.Column('severity', sa.SMALLINT(), nullable=False),
+        sa.Column('updated_dttm', postgresql.TIMESTAMP(), server_default='now()', nullable=False),
+        sa.PrimaryKeyConstraint('vulnerability_type_id')
+    )
+    op.create_index(op.f('ix_vulnerability_type_name'), 'vulnerability_type', ['name'], unique=True)
+    op.create_index(op.f('ix_vulnerability_type_severity'), 'vulnerability_type', ['severity'], unique=False)
+    op.execute("""
+        CREATE TRIGGER host_audit
+        AFTER INSERT OR UPDATE OR DELETE ON vulnerability_type
+        FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+    """)
+
+    op.create_table('vulnerability',
+        sa.Column('vulnerability_id', sa.Integer(), nullable=False),
+        sa.Column('vulnerability_type_id', sa.Integer(), nullable=False),
+        sa.Column('status', sa.Enum('found', 'confirmed', 'solved', 'false positive', name='vulnerability_status_enum'), nullable=False),
+        sa.Column('found_by', sa.TEXT(), nullable=False),
+        sa.Column('found_dttm', postgresql.TIMESTAMP(), server_default='now()', nullable=False),
+        sa.Column('confirmed_by', sa.TEXT(), nullable=True),
+        sa.Column('confirmed_dttm', postgresql.TIMESTAMP(), nullable=True),
+        sa.Column('solved_dttm', postgresql.TIMESTAMP(), nullable=True),
+        sa.Column('host_id', sa.Integer(), nullable=True),
+        sa.Column('path_id', sa.Integer(), nullable=True),
+        sa.Column('machine_id', sa.Integer(), nullable=True),
+        sa.Column('updated_dttm', postgresql.TIMESTAMP(), server_default='now()', nullable=False),
+        sa.ForeignKeyConstraint(['host_id'], ['host.host_id'], ),
+        sa.ForeignKeyConstraint(['machine_id'], ['machine.machine_id'], ),
+        sa.ForeignKeyConstraint(['path_id'], ['path.path_id'], ),
+        sa.ForeignKeyConstraint(['vulnerability_type_id'], ['vulnerability_type.vulnerability_type_id'], ),
+        sa.PrimaryKeyConstraint('vulnerability_id')
+    )
+    op.execute("""
+        CREATE TRIGGER host_audit
+        AFTER INSERT OR UPDATE OR DELETE ON vulnerability
+        FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+    """)
+
+
+def downgrade():
+    op.drop_table('vulnerability')
+    op.drop_index(op.f('ix_vulnerability_type_severity'), table_name='vulnerability_type')
+    op.drop_index(op.f('ix_vulnerability_type_name'), table_name='vulnerability_type')
+    
+    op.drop_table('vulnerability_type')
+    
+    op.execute('drop type vulnerability_status_enum')
