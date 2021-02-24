c390d37a29c2923676a8dea200e1a5d650a4d287
Cainotis <cainotis@gmail.com>
2021-02-24 18:53:54 -0300

    Added Amass service
    - finished dockerfile and entrypoint
    - added Controller that will:
    	- read and run amass -noalt to all roots in the targets.json
    	- check if the domain really exists by looking if it responds
    	- add new machines and hosts to the database
    	- update the old hosts to database

--- docker/amass/docker-entrypoint.sh
+++ docker/amass/docker-entrypoint.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 
-python3 main.py
+# copy all requirements to one file
+cp requirements.txt all_requirements.txt
+sed -i -e '$a\' all_requirements.txt
+cat commons/requirements.txt >> all_requirements.txt
 
-# while :
-# do
-# 	sleep 1000
-# done
+echo "--------------------------"
+echo "----- Installing Project Dependencies -----"
+if [ -f installed_requirements.txt ]; then
+  echo "----- Dependencies previously installed. Checking if we need to install again -----"
+  DIFF=$(diff all_requirements.txt installed_requirements.txt) 
+  if [ "$DIFF" != "" ] 
+  then
+    echo "----- Dependencies modified. Reinstalling -----"
+    pip freeze | xargs pip uninstall -y
+    pip install -r all_requirements.txt
+    mv all_requirements.txt installed_requirements.txt
+  else
+    echo "----- Dependencies not modified -----"
+  fi
+else
+  echo "----- No dependencies previously installed. Installing -----"
+  pip install -r all_requirements.txt
+  mv all_requirements.txt installed_requirements.txt
+fi
+
+echo "Start processing"
+
+python3 main.py
--- recon/amass/main.py
+++ recon/amass/main.py
@@ -1,8 +1,46 @@
-import subprocess
+import logging
+import sqlalchemy
+import sqlalchemy.orm
+from configparser import ConfigParser
 
 
-result = subprocess.run(["amass", "enum", "-src", "-noalts", "-d", "imesec.ime.usp.br", "-json", "scr/output.json", "-log", "logs/amass.log"], 
-                        stdout=subprocess.PIPE).stdout.decode("utf-8")
+from commons.alchemyrepository import \
+	HostRepository, \
+	MachineRepository
 
-print(result)
+from scr import Controller
 
+
+def main():
+	logger = logging.getLogger("AMASS")
+	
+	config = ConfigParser()
+	config.read("commons/config.ini")
+
+	engine = sqlalchemy.create_engine(
+		'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'.format(
+			db_user=config["DB"]["user"],
+			db_pass=config["DB"]["pass"],
+			db_host=config["DB"]["host"],
+			db_port=config["DB"]["port"],
+			db_name=config["DB"]["name"]
+		)
+	)
+
+	session_maker = sqlalchemy.orm.sessionmaker(engine)
+	session = session_maker(autoflush=False)
+
+	machine_repository = MachineRepository(session)
+	host_repository = HostRepository(session)
+
+	controller = Controller(
+		host_repository=host_repository,
+		machine_repository=machine_repository,
+		logger=logger
+	)
+	controller.execute()
+
+	session.commit()
+
+if __name__ == '__main__':
+	main()
