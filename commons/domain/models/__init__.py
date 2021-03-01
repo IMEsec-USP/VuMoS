b59cedb8ac8fabcc0269aa8a89945f0ef46ebc5a
Cainotis <cainotis@gmail.com>
2021-03-01 14:25:29 -0300

    Added Config and Nmap tables
    - added tables on mapper
    - added models to domain
    - added mapping table-class to mapper

--- commons/domain/models/__init__.py
+++ commons/domain/models/__init__.py
@@ -1,5 +1,7 @@
+from ._Config import Config
 from ._Host import Host
 from ._Machine import Machine
+from ._Nmap import Nmap
 from ._Path import Path
 from ._Vulnerability import Vulnerability
 from ._VulnerabilityStatusEnum import VulnerabilityStatusEnum
