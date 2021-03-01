4783c28792619f22285bf882800d539b621df8b2
Cainotis <cainotis@gmail.com>
2021-03-01 13:46:54 -0300

    Change from using config.ini to ENV
    - updated alembic/env.py to read from env
    - updated amass/main.py to read from env
    - removed unused file docker/build.sh
    - added data folder to gitignore
    
    Signed-off-by: Cainotis <cainotis@gmail.com>

--- recon/amass/main.py
+++ recon/amass/main.py
@@ -1,7 +1,7 @@
+import os
 import logging
 import sqlalchemy
 import sqlalchemy.orm
-from configparser import ConfigParser
 
 
 from commons.alchemyrepository import \
@@ -15,16 +15,13 @@ from src import Controller
 def main():
 	logger = logging.getLogger("AMASS")
 	
-	config = ConfigParser()
-	config.read("commons/config.ini")
-
 	engine = sqlalchemy.create_engine(
 		'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'.format(
-			db_user=config["DB"]["user"],
-			db_pass=config["DB"]["pass"],
-			db_host=config["DB"]["host"],
-			db_port=config["DB"]["port"],
-			db_name=config["DB"]["name"]
+			db_user=os.environ.get("DB_USER"),
+			db_pass=os.environ.get("DB_PASS"),
+			db_name=os.environ.get("DB_NAME"),
+			db_host=os.environ.get("DB_HOST"),
+			db_port=os.environ.get("DB_PORT")
 		)
 	)
 
