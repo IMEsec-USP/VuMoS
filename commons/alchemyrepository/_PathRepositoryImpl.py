34265ac43b8b6f9836dfad36861ba92216162373
Cainotis <cainotis@gmail.com>
2021-03-05 17:02:49 -0300

    removed update in path on safe_add to not have problens on the crawler

--- commons/alchemyrepository/_PathRepositoryImpl.py
+++ commons/alchemyrepository/_PathRepositoryImpl.py
@@ -29,9 +29,6 @@ class PathRepository(definition):
 		if p is None:
 			self.session.add(path)
 		else:
-			p.access_dttm = datetime.now()
-			p.updated_dttm = datetime.now()
-			p.times_offline = 0
 			path = p
 		self.session.flush()
 		return path
