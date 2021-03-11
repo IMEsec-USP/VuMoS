edf2feb7f24337c9821a0a2034b4d7ea57265f67
Cainotis <cainotis@gmail.com>
2021-03-11 10:27:19 -0300

    Deleted unused files and remove commented code
    Signed-off-by: Cainotis <cainotis@gmail.com>

--- crawler/docker-entrypoint.sh
+++ crawler/docker-entrypoint.sh
@@ -5,10 +5,4 @@ until PGPASSWORD=$DB_PASS psql -h "$DB_HOST" -U "postgres" -c '\q'; do
   sleep 1
 done
 
-# while :
-# do 
-# 	sleep 10000
-# done
-
 python -m scrapy crawl sqlsearch -o /dev/stdout:json
-# python -m scrapy crawl sqlsearch -o output.json:json
